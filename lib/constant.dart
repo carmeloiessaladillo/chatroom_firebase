861667b93310ea5e2f64904897c0f69d5c4c7eb0
Carmelo Villegas cruz <meloville@gmail.com>
2021-02-04 21:07:14 +0100

    Initial commit

---
+++ lib/constant.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+//Definimos los valores constantes de nuestra APP
+//Colores
+const Color C_ORANGE = Color(0xFFFEC20A);
+const Color C_RED = Color(0xFFB22D34);
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'constant.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: C_ORANGE,
+    );
+  }
+}
